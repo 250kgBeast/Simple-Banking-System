7df90f312f10de078094d02410426d7981d0778f
250kgBeast <bakarov@ukr.net>
2020-07-09 19:13:04 +0300

    added functionality

--- BankSystem.py
+++ BankSystem.py
@@ -18,10 +18,12 @@ else:
 
 class Bank:
     LOGIN_MENU_LOOP = False
+    _logged_in_number = 0
+    _logged_in_balance = 0
 
-    def __create_account(self):
-        card_number = self.__generate_card_number()
-        password = self.__generate_pin()
+    def _create_account(self):
+        card_number = self._generate_card_number()
+        password = self._generate_pin()
         cur.execute('INSERT INTO card (number, pin) VALUES (?, ?)', (card_number, password))
         conn.commit()
         print('Your card has been created')
@@ -29,11 +31,11 @@ class Bank:
         print(f'Your card PIN:\n{password}\n')
 
     @staticmethod
-    def __generate_pin():
+    def _generate_pin():
         return f"{randint(0000, 9999):04}"
 
     @staticmethod
-    def __generate_card_number():
+    def _generate_card_number():
         account_identifier = [randint(0, 9) for i in range(9)]
         account_identifier_copy = account_identifier.copy()
         account_identifier_copy = [account_identifier_copy[i] * 2 if i % 2 == 0
@@ -47,46 +49,53 @@ class Bank:
                 break
         return card_number
 
-    def __do_transfer(self):
+    def _do_transfer(self):
         pass
 
-    def __balance(self):
-        pass
+    def _balance(self):
+        print(f'Balance: {self._logged_in_balance}\n')
 
-    def __add_income(self):
-        pass
+    def _add_income(self):
+        logged_in_user_income_input = input('Enter income:\n')
+        cur.execute('UPDATE card SET balance = balance + ? WHERE number = ?',
+                    (logged_in_user_income_input, self._logged_in_number))
+        conn.commit()
+        self._logged_in_balance += int(logged_in_user_income_input)
+        print('Income was added!\n')
 
-    def __close_account(self):
-        pass
+    def _close_account(self):
+        cur.execute('DELETE FROM card WHERE number = ?', (self._logged_in_number, ))
+        conn.commit()
+        print('The account has been closed!\n')
 
-    def __login(self):
-        cur.execute('SELECT number, pin FROM card')
-        print('Enter your card number:')
-        user_input_card_number = input()
-        print('Enter your PIN:')
-        user_input_pin = input()
-        for card_number, pin in cur.fetchall():
-            if user_input_card_number == card_number and user_input_pin == pin:
-                print('You have successfully logged in!\n')
-                self.LOGIN_MENU_LOOP = True
-                break
+    def _login(self):
+        user_input_card_number = input('Enter your card number:\n')
+        user_input_pin = input('Enter your PIN:\n')
+        cur.execute('SELECT number, balance FROM card WHERE number = ? AND pin = ?',
+                    (user_input_card_number, user_input_pin))
+        logged_in_user_data = cur.fetchone()
+        if logged_in_user_data:
+            print('You have successfully logged in!\n')
+            self.LOGIN_MENU_LOOP = True
+            self._logged_in_number, self._logged_in_balance = logged_in_user_data
         else:
             print('Wrong card number or PIN!\n')
 
-    def __login_menu(self):
+    def _login_menu(self):
         self.LOGIN_MENU_LOOP = True
         print('1. Balance\n2. Add income\n3. Do transfer\n4. Close account\n5. Log out\n0. Exit')
         user_input = input()
         if user_input == '1':
-            self.__balance()
+            self._balance()
         elif user_input == '2':
-            self.__add_income()
+            self._add_income()
         elif user_input == '3':
-            self.__do_transfer()
+            self._do_transfer()
         elif user_input == '4':
-            self.__close_account()
+            self._close_account()
         elif user_input == '5':
             print('You have successfully logged out!')
+            self._logged_in_balance, self._logged_in_number = 0, 0
             self.LOGIN_MENU_LOOP = False
         elif user_input == '0':
             exit()
@@ -96,11 +105,11 @@ class Bank:
             print('1. Create an account\n2. Log into account\n0. Exit')
             user_input = input()
             if user_input == '1':
-                self.__create_account()
+                self._create_account()
             elif user_input == '2':
-                self.__login()
+                self._login()
                 while self.LOGIN_MENU_LOOP:
-                    self.__login_menu()
+                    self._login_menu()
             elif user_input == '0':
                 exit()
 
