f6345f728c2354a3c7927f3700b25c0340a57f49
250kgBeast <bakarov@ukr.net>
2020-07-09 14:08:39 +0300

    refactored in oop

--- BankSystem.py
+++ BankSystem.py
@@ -5,89 +5,77 @@ conn = sqlite3.connect('bank.sqlite')
 cur = conn.cursor()
 
 
-MAIN_MENU_LOOP = True
-LOGIN_MENU_LOOP = False
+class Bank:
+    LOGIN_MENU_LOOP = False
 
+    def __create_account(self):
+        card_number = self.__generate_card_number()
+        password = self.__generate_pin()
+        cur.execute('INSERT INTO card (number, pin) VALUES (?, ?)', (card_number, password))
+        conn.commit()
+        print('Your card has been created')
+        print(f'Your card number:\n{card_number}')
+        print(f'Your card PIN:\n{password}\n')
 
-def create_account():
-    card_number = generate_card_number()
-    password = generate_password()
-    cur.execute('INSERT INTO card (number, pin) VALUES (?, ?)', (card_number, password))
-    conn.commit()
-    print('Your card has been created')
-    print(f'Your card number:\n{card_number}')
-    print(f'Your card PIN:\n{password}\n')
+    @staticmethod
+    def __generate_pin():
+        return f"{randint(0000, 9999):04}"
 
+    @staticmethod
+    def __generate_card_number():
+        account_identifier = [randint(0, 9) for i in range(9)]
+        account_identifier_copy = account_identifier.copy()
+        account_identifier_copy = [account_identifier_copy[i] * 2 if i % 2 == 0
+                                   else account_identifier_copy[i] for i in range(len(account_identifier_copy))]
+        account_identifier_copy = [item - 9 if item > 9 else item for item in account_identifier_copy]
+        dig_sum = sum(account_identifier_copy) + 8
+        card_number = '400000' + ''.join(map(str, account_identifier))
+        for i in range(0, 10):
+            if (dig_sum + i) % 10 == 0:
+                card_number += str(i)
+                break
+        return card_number
 
-def generate_password():
-    return f"{randint(0000, 9999):04}"
+    def login(self):
+        cur.execute('SELECT number, pin FROM card')
+        print('Enter your card number:')
+        user_input_card_number = input()
+        print('Enter your PIN:')
+        user_input_pin = input()
+        for card_number, pin in cur.fetchall():
+            if user_input_card_number == card_number and user_input_pin == pin:
+                print('You have successfully logged in!\n')
+                self.LOGIN_MENU_LOOP = True
+                break
+        else:
+            print('Wrong card number or PIN!\n')
 
+    def __login_menu(self):
+        self.LOGIN_MENU_LOOP = True
+        print('1. Balance\n2. Log out\n0. Exit')
+        user_input = input()
+        if user_input == '1':
+            print('Balance: 0\n')
+        elif user_input == '2':
+            print('You have successfully logged out!')
+            self.LOGIN_MENU_LOOP = False
+        elif user_input == '0':
+            exit()
 
-def generate_card_number():
-    BIN = '400000'
-    account_identifier = []
-    check_sum = 0
-    for i in range(0, 9):
-        account_identifier.append(randint(0, 9))
-    account_identifier_copy = account_identifier.copy()
-    for i in range(0, 9, 2):
-        account_identifier_copy[i] *= 2
-    for i in range(0, 9):
-        if account_identifier_copy[i] > 9:
-            account_identifier_copy[i] -= 9
-    dig_sum = sum(account_identifier_copy) + 8
-    for i in range(0, 10):
-        if (dig_sum + i) % 10 == 0:
-            check_sum += i
-            break
-    card_number = BIN + ''.join(map(str, account_identifier)) + str(check_sum)
-    return card_number
-
-
-def login():
-    global LOGIN_MENU_LOOP
-    cur.execute('SELECT number, pin FROM card')
-    print('Enter your card number:')
-    user_input_card_number = input()
-    print('Enter your PIN:')
-    user_input_password = input()
-    for card_number, password in cur.fetchall():
-        if user_input_card_number == card_number and user_input_password == password:
-            print('You have successfully logged in!\n')
-            LOGIN_MENU_LOOP = True
-            break
-    else:
-        print('Wrong card number or PIN!\n')
-
-
-def login_menu():
-    global LOGIN_MENU_LOOP, MAIN_MENU_LOOP
-    print('1. Balance\n2. Log out\n0. Exit')
-    user_input = input()
-    if user_input == '1':
-        print('Balance: 0\n')
-    elif user_input == '2':
-        print('You have successfully logged out!')
-        LOGIN_MENU_LOOP = False
-    elif user_input == '0':
-        LOGIN_MENU_LOOP = False
-        MAIN_MENU_LOOP = False
-
-
-def menu():
-    global MAIN_MENU_LOOP
-    print('1. Create an account\n2. Log into account\n0. Exit')
-    user_input = input()
-    if user_input == '1':
-        create_account()
-    elif user_input == '2':
-        login()
-        while LOGIN_MENU_LOOP:
-            login_menu()
-    elif user_input == '0':
-        MAIN_MENU_LOOP = False
+    def menu(self):
+        while True:
+            print('1. Create an account\n2. Log into account\n0. Exit')
+            user_input = input()
+            if user_input == '1':
+                self.__create_account()
+            elif user_input == '2':
+                self.login()
+                while self.LOGIN_MENU_LOOP:
+                    self.__login_menu()
+            elif user_input == '0':
+                exit()
 
 
 if __name__ == '__main__':
-    while MAIN_MENU_LOOP:
-        menu()
+    bank = Bank()
+    bank.menu()
